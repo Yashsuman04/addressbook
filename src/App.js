6175fbf42c4bd266a9049cec8dc2dd833af8990e
Yashsuman04 <109915151+Yashsuman04@users.noreply.github.com>
2025-03-10 15:46:51 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { ContactProvider } from "./context/ContactContext";
+import AddContact from "./components/AddContact";
+import ContactList from "./components/ContactList";
+import { useState } from "react";
+
+
+function App() {
+  return (
+    <ContactProvider>
+      <div className="container py-5">
+        <h1 className="text-center mb-5">Address Book</h1>
+        <div className="row">
+          <div className="col-md-4">
+            <AddContact />
+          </div>
+          <div className="col-md-8">
+            <ContactList />
+          </div>
+        </div>
+      </div>
+    </ContactProvider>
+  );
+}
+
+export default App;
